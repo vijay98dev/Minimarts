420d303b8b6d698de568e5fb82212e81e9b3eb08
vijay98dev <vijay89dev@gmail.com>
2023-08-24 16:04:36 +0530

    Search has been done. Now doing wishlist-a

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('store/',views.store,name='store'),
     path('store/<category_slug>/',views.store,name='product_by_slug'),
     path('store/<category_slug>/<product_slug>/',views.product_details,name='product_details'),
+    path('search/',views.search,name='search'),
 ]
--- store/views.py
+++ store/views.py
@@ -26,6 +26,11 @@ def store(request,category_slug=None):
 
 def product_details(request,category_slug,product_slug):
     try:
+        single_product=ProductImage.objects.get(product__category__slug=category_slug,product_size__slug=product_slug)
+        # print(single_product)
+        data=ProductSize.objects.all().filter(slug=product_slug)
+        print(data)
+        in_cart=CartItems.objects.filter(cart__cart_id=_cart_id(request),product=single_product).exists()
         single_product=ProductImage.objects.filter( product__category__slug=category_slug,product_size__slug=product_slug)
         # data=ProductImage.objects.filter(product_size=single_product)
         # in_cart=CartItems.objects.filter(cart__cart_id=_cart_id(request),product__product_size=single_product).exists()
@@ -33,8 +38,23 @@ def product_details(request,category_slug,product_slug):
         raise Http404("Product not found")
     context={
         'single_product':single_product,
+        'data':data,
+        'in_cart':in_cart,
         # 'data':data,
         # 'in_cart':in_cart,
     }
     return render(request,'user/product_details.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword=request.GET['keyword']
+        print(keyword,'2368726348621938691273412937498')
+        if keyword:
+            print(keyword,'2368726348621938691273412937498')
+            product=ProductImage.objects.filter(Q(product__product_name__icontains=keyword) | Q(product__description__icontains=keyword) | Q(product__category__category_name__icontains=keyword) | Q(product_size__price__icontains=keyword))
+        context={
+            'products':product
+        }
+    return render(request,'user/search.html',context)
 
